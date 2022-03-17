ba8a9466aa98557484fb9da5dc6abb2d75f953d2
James Vanlalpeka <66527505+JamesVLpeka@users.noreply.github.com>
2022-03-17 18:34:38 +0530

    Add files via upload

---
+++ Code/index_search.js
@@ -0,0 +1,94 @@
+function search(src)
+{
+
+    switch (src)
+    {
+        case "feedback": window.open("feedback.html");
+        break;
+
+        case "Feedback": window.open("feedback.html");
+        break;
+
+        case "FEEDBACK": window.open("feedback.html");
+        break;
+
+        case "Tips": window.open("tips.html");
+        break;
+
+        case "tips": window.open("tips.html");
+        break;
+
+        case "TIPS": window.open("tips.html");
+        break;
+
+        case "Gardening Tips": window.open("garden.html");
+        break;
+
+        case "Gardening tips": window.open("garden.html");
+        break;
+
+        case "gardening tips": window.open("garden.html");
+        break;
+
+        case "GARDENING TIPS": window.open("garden.html");
+        break;
+
+        case "Tools and Types": window.open("tools_and_types.html");
+        break;
+
+        case "Tools and Type": window.open("tools_and_types.html");
+        break;
+
+        case "tools and type": window.open("tools_and_types.html");
+        break;
+
+        case "Tool and Types": window.open("tools_and_types.html");
+        break;
+
+        case "tools and types": window.open("tools_and_types.html");
+        break;
+
+        case "TOOLS AND TYPES": window.open("tools_and_types.html");
+        break;
+
+        case "tools and types": window.open("tools_and_types.html");
+        break;
+
+        case "Types of Gardening": window.open("types_of_gardening.html");
+        break;
+
+        case "Types": window.open("types_of_gardening.html");
+        break;
+
+        case "types of gardening": window.open("types_of_gardening.html");
+        break;
+
+        case "TYPES OF GARDENING": window.open("types_of_gardening.html");
+        break;
+
+        case "Type of Gardening": window.open("types_of_gardening.html");
+        break;
+
+        case "Shop": window.open("shop.html");
+        break;
+
+        case "shop": window.open("shop.html");
+        break;
+
+        case "SHOP": window.open("shop.html");
+        break;
+
+        case "Shopping": window.open("shop.html");
+        break;
+
+        case "shopping": window.open("shop.html");
+        break;
+
+        case "SHOPPING": window.open("shop.html");
+        break;
+
+        default: alert("In the Search Bar you can search the pages of this website.");
+
+    }
+
+}
